26669bb2a8fa24695ca397b5b4264498ed2ab7ce
ciuzaak <as460230379@hotmail.com>
2023-04-10 03:56:02 +0800

    update bard_utils

--- bard_utils.py
+++ bard_utils.py
@@ -6,20 +6,15 @@ class Bard:
     def __init__(self, id=None) -> None:
         self.mode = "bard"
         self.client = Chatbot(config.bard_api)
-        self.prompt = ""
         self.id = id
-        self.HUMAN_PROMPT = "\n\nHuman:"
-        self.AI_PROMPT = "\n\nAssistant:"
 
     def reset(self):
-        self.prompt = ""
+        self.client.conversation_id = ""
+        self.client.response_id = ""
+        self.client.choice_id = ""
 
     def get_mode(self):
         return self.mode
 
     def send_message(self, message):
-        self.prompt = f"{self.prompt}{self.HUMAN_PROMPT} {message}{self.AI_PROMPT}"
-        response = self.client.ask(self.prompt)['content']
-        self.prompt = f"{self.prompt} {response}"
-
-        return response
+        return self.client.ask(message)['content']
